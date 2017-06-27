a580b055b10a47800f4732a0e15002ea533e0f3d
Eliya Elon <elon.eliya@gmail.com>
2017-06-27 19:02:21 +0300

    Initial commit

---
+++ imports/ui/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import TitleBar from './TitleBar.js';
+import AddPlayer from './AddPlayer.js';
+import PlayerList from './PlayerList.js';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <TitleBar title={this.props.title} subtitle='Made by Eliya'/>
+        <div className="wrapper">
+          <PlayerList players={this.props.players}/>
+          <AddPlayer/>
+        </div>
+      </div>
+    );
+  }
+}
+App.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  players: React.PropTypes.array.isRequired
+};
